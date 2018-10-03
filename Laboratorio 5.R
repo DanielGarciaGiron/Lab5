17d608e8edc7fd46d8b1d28f9a24ca45fa3daa59
Rodrigo Chavez <rodrich266@gmail.com>
2018-10-03 11:56:14 -0600

    Lowercase

---
+++ Laboratorio 5.R
@@ -0,0 +1,28 @@
+#------------------------------------------------------------#
+# Laboratorio 5
+# Grupo 1
+#------------------------------------------------------------#
+
+#----- Instalación e importación de librerias necesarias para correr el programa ----#
+for (libreria in c("stringr","tm", "R.utils","openNLP","qdap","RWeka","ggplot2", "dplyr","tidyr","wordcloud")) {
+  if (!require(libreria, character.only=T)) {
+    install.packages(libreria)
+    library(libreria, character.only=T)
+  }
+}
+
+#---------------------- Lectura de Datos --------------------------------------------#
+Datos <- read.csv("./datos/GrammarandProductReviews.csv", stringsAsFactors = F)
+
+#Se realiza un vector de los datos y se convierten en volátiles para cambiar su contenido
+VectorDatos <- VectorSource(Datos)
+DatosLimpios <- VCorpus(VectorDatos)
+
+#Preparar transformación
+DatosLimpios <- tm_map(DatosLimpios, content_transformer(function(x) iconv(x, to='UTF-8', sub='byte')))
+
+#Se transforman los caracteres a minúsculas
+DatosLimpios<-tm_map(DatosLimpios, content_transformer(tolower))
+
+#Se eliminan los espacios en blanco adicionales
+DatosLimpios<-tm_map(DatosLimpios, content_transformer(stripWhitespace))
--- Laboratorio5.R
+++
@@ -1,8 +0,0 @@
-for (libreria in c("stringr","tm", "R.utils","openNLP","rJava","qdap","RWeka","ggplot2","stylo")) {
-  if (!require(libreria, character.only=T)) {
-    install.packages(libreria)
-    library(libreria, character.only=T)
-  }
-}
-
-Datos <- read.csv("./datos/GrammarandProductReviews.csv", stringsAsFactors = F)
